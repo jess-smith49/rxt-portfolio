dd4178adf31dd1c0a9b2ae7b2cd253a9f6b69076
jess-smith49 <js.jessicasmith774@gmail.com>
2021-01-29 22:17:55 -0600

    working on styling and insertion of images for projects

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,20 +1,27 @@
 function About() {
     return(
         <section>
-            <h1 className = "title">ABOUT ME</h1>
-            <div>
-                <div className = "information-box">
+            <h1 className = "title d-flex justify-content-center">ABOUT ME</h1>
+    
+            <div className = "d-flex justify-content-center">
+                <div className = "information-box w-50">
+                    <div className = "d-flex justify-content-center card-title">Background</div>
                     I am senior at the University of Wisconsin Oshkosh studying Information Systems and in the process
                     of completing my Full Stack Web Development Certificate from UW-Extended. I have been an intern at Schreiber Foods in Green Bay
                     as an IS QA Analyst since July 2020 and I am an SAP tutor at UWO. 
                 </div>
         <br />
-                <div className = "information-box">
-                    My interests in this field include web development, system analysis and desing, business analysis, and business
-                    process re-engineering.
+                <div className = "information-box w-50">
+                    <div className = "d-flex justify-content-center card-title">Interests</div>
+                    <li>Business Process Re-engineering</li>
+                    <li>System Analysis and Design</li>
+                    <li>Software Development</li>
+                    <li>ERP Systems</li>
+                    <li>Business Analysis</li>
                 </div>
         <br />
-                <div className = "information-box">
+                <div className = "information-box w-50">
+                    <div className = "d-flex justify-content-center card-title">Extras</div>
                     Now that the intro is out of the way!
                     I love to travel. I have been to Canada, Italy, the United Kingdom twice and Jamaica. Next on my list is Greece,
                     someday. I enjoy reading books of all sorts but I also love my netflix TV shows. A random fun fact about me is that 
@@ -22,7 +29,9 @@ function About() {
                     in aerial yoga and it is on my to-do list to take some classes this year. I love finding hole in the wall bar and grills 
                     not in the city and making new friends to share experiences with.
                 </div>
+                
             </div>
+
         </section>
     )
 }
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,24 +4,21 @@ function Projects(){
         <div>
             <div class="container">
                 <div class="row">
-                    <div class="col-sm">
-                    <a href="https://github.com/jess-smith49/budget-tracker" class = "budget-tracker">Budget Tracker</a>
-                            <div>
-                                <h3>Budget Tracker</h3>
+                    <div class="col-sm" >
+                    <a href="https://github.com/jess-smith49/budget-tracker" >Budget Tracker</a>
+                            <div> 
                                 <span>PWA</span>
                             </div>
                     </div>
                     <div class="col-sm e-comm">
-                    <a href="https://github.com/jess-smith49/e-comm_backend" class = "e-comm"></a>
+                    <a href="https://github.com/jess-smith49/e-comm_backend" class = "e-comm">E-Commerce Backend</a>
                             <div>
-                                <h3>E-Commerce</h3>
                                 <span>PWA</span>
                             </div>
                     </div>
                     <div class="col-sm social-network">
-                    <a href="https://github.com/jess-smith49/social-network" class = "social-network"></a>
+                    <a href="https://github.com/jess-smith49/social-network" class = "social-network">Social Network API</a>
                             <div>
-                                <h3>Social Network</h3>
                                 <span>MongoDB</span>
                             </div>
                     </div>
@@ -31,23 +28,20 @@ function Projects(){
             <div class="container">
                 <div class="row">
                     <div class="col-sm regex">
-                    <a href="https://gist.github.com/jess-smith49/bae1cd32a591a4b3972efb9da00d4c96" class = "social-network"></a>
+                    <a href="https://gist.github.com/jess-smith49/bae1cd32a591a4b3972efb9da00d4c96" class = "social-network">Regex</a>
                             <div>
-                                <h3>Regex</h3>
                                 <span>Tutorial</span>
                             </div>
                     </div>
                     <div class="col-sm express-notes">
-                    
+                    <a href="https://github.com/jess-smith49/express-notes">Note-Taker</a> 
                             <div>
-                                <h3>Note-Taker</h3>
-                     <a href="https://github.com/jess-smith49/express-notes">Express-Notes</a>              
+                                <span>Express</span>
                             </div>
                     </div>
                     <div class="col-sm read-me">
-                    <a href="https://github.com/jess-smith49/readme-node" class = "social-network"></a>
+                    <a href="https://github.com/jess-smith49/readme-node" class = "social-network">Readme Generator</a>
                             <div>
-                                <h3>Read-me Generator</h3>
                                 <span>Express</span>
                             </div>
                     </div>
